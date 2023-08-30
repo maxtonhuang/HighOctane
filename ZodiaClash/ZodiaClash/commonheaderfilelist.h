07c75862ca6f226957d149cf414f3e7ec8b3ed18
KWenYuan <93873311+KWenYuan@users.noreply.github.com>
2023-08-30 16:08:34 +0800

    Added commonheaderfilelist.h
    Added the vectors 2 and vectors 3 for vmath.h

---
+++ ZodiaClash/ZodiaClash/commonheaderfilelist.h
@@ -0,0 +1,23 @@
+/*
+\copyright
+		All content(C) 2023 DigiPen Institute of Technology Singapore.All rights
+		reserved.Reproduction or disclosure of this file or its contents without the prior
+		written consent of DigiPen Institute of Technology is prohibited.
+*/
+/*!
+@file		header.h
+@author		Koh Wen Yuan
+@Email		k.wenyuan@digipen.edu
+@course		CSD 2401
+@section	Section A
+@date		30 August 2023
+@brief		This file contains the compilation for all the COMMON header files
+*//*______________________________________________________________________*/
+#pragma once
+/*----------------------------------------------------
+					WARNING!!!!!
+			ONLY ADD COMMON HEADER FILES
+			ELSE COMPILATION WILL BE VERY SLOW
+----------------------------------------------------*/
+
+// Common header files to be put into here
