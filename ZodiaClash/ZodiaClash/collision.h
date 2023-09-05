247abe346a49a4fd63d6c57a80d742f1758a9e75
Zhen Rong <chuazhenrong2k@gmail.com>
2023-09-06 01:47:21 +0800

    physics sortations
    sorted prev code into respective files
    - physics to consider step/debugdraw
    - integrate vs update(step)
    - collision to consider converting physicsbody to class for component usage, rest of collision "library"

--- ZodiaClash/ZodiaClash/collision.h
+++ ZodiaClash/ZodiaClash/collision.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <vector>
+#include "vmath.h"
 
 namespace physics {
 
@@ -7,5 +9,32 @@ namespace physics {
 	* - collision detection
 	* - basic shape data for now
 	*/
+
+	struct PhysicsBody {
+
+		//position will be the center of the object 
+		vmath::Vector2 position, velocity{ 0,0 }, acceleration{ 0,0 }, size{};
+		float bounciness{ 0.5f }; // after collisions
+		float friction{ 0.2f };    // Slowing down due to surfaces
+		float mass;
+		bool isStatic;     // Indicates if the body is movable
+
+		// Constructor
+		PhysicsBody(float x, float y, float width, float height, float mass, bool isStatic = false)
+			: position({ x, y }), mass(mass), size({ width, height }), isStatic(isStatic) {};
+
+		bool CheckCollision(const PhysicsBody& otherBody);// check collision with another body
+
+	};
+
+	class CollisionManager
+	{
+	public:
+		void CollisionDetectionAndResponse();
+		static bool CheckCollision(const PhysicsBody& bodyA, const PhysicsBody& bodyB); // check collision with 2 bodies
+		void CollisionResponse(PhysicsBody& bodyA, PhysicsBody& bodyB);
+
+		std::vector<PhysicsBody> bodies; // store all the bodies
+	};
 
 }
--- ZodiaClash/ZodiaClash/physics.h
+++ ZodiaClash/ZodiaClash/physics.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "vmath.h"
+#include "collision.h"
 
 namespace physics {
 
@@ -12,31 +13,37 @@ namespace physics {
 	* - consider debug drawing step
 	*/
 
-	struct PhysicsBody {
-		//position will be the center of the object 
-		vmath::Vector2 position, velocity{ 0,0 }, acceleration{ 0,0 }, size{};
-		float bounciness{ 0.5f }; // after collisions
-		float friction{ 0.2f };    // Slowing down due to surfaces
-		float mass;
-		bool isStatic;     // Indicates if the body is movable
+	class PhysicsManager {
 
-		// Constructor
-		PhysicsBody(float x, float y, float width, float height, float mass, bool isStatic = false)
-			: position({ x, y }), mass(mass), size({ width, height }), isStatic(isStatic) {};
-
-		bool CheckCollision(const PhysicsBody& otherBody);// check collision with another body
-
-	};
-
-	class PhysicsEngine {
-
+	// new ver. arrangment
 	public:
-		std::vector<PhysicsBody> bodies; // store all the bodies
+		void Init(); //initializes 1x instance of physics and relevant (global?) variables
 		void Update(float deltaTime);
 		void Update(PhysicsBody& body, float deltaTime);
-		void ExternalForces(PhysicsBody& body, float deltaTime);
-		void CollisionDetectionAndResponse();
-		static bool CheckCollision(const PhysicsBody& bodyA, const PhysicsBody& bodyB); // check collision with 2 bodies
-		void CollisionResponse(PhysicsBody& bodyA, PhysicsBody& bodyB);
+		void DetectCollision(float deltaTime);
+		CollisionManager collision;
+
+	//attributes (some overlap with body, need further clarif.)
+	public:
+		vmath::Vector2 gravity;
+		float maxVelocity;
+		float maxVelocitySq;
+
+		//note: PenetrationEpsilon and PenetrationResolvePercentage in sample, to confirm further usage
+
+
+	//// old ver. arrangement
+	//public:
+	//	std::vector<PhysicsBody> bodies; // store all the bodies
+	//	void Update(float deltaTime);
+	//	void Update(PhysicsBody& body, float deltaTime);
+	//	void ExternalForces(PhysicsBody& body, float deltaTime);
+	//	void CollisionDetectionAndResponse();
+	//	static bool CheckCollision(const PhysicsBody& bodyA, const PhysicsBody& bodyB); // check collision with 2 bodies
+	//	void CollisionResponse(PhysicsBody& bodyA, PhysicsBody& bodyB);
+
 	};
+
+	// global pointer
+	extern PhysicsManager* PHYSICS;
 }
