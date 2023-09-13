58312378729940544985ec0d51a6dda0aade6b0a
kai-alexander <sandofgarra@gmail.com>
2023-09-14 00:32:34 +0800

    Cleaning up of function

--- ZodiaClash/ZodiaClash/Serialization.cpp
+++ ZodiaClash/ZodiaClash/Serialization.cpp
@@ -51,45 +51,12 @@ void StreamRead(Serializer& stream, std::string& str)
 void Serializer::SerializeCSV(const std::string& file) {
 	Serializer stream;
 	bool isFileOpen = stream.Open(file);
-	Assert(isFileOpen, "Unable to open file");
-	std::string line,header;
+	Assert(!isFileOpen, "Unable to open file");
+	std::string line, header;
 	PseudoCharacter characterStats;
-	//if (stream.IsGood()) {
-	//	std::getline(stream.stream, line);
-	//	std::stringstream ss(line);
-
-	//	while (std::getline(ss, header, ',')) {
-
-	//	}
-	//}
-
-	/*if (stream.IsGood()) {
-		while (std::getline(stream.stream, line)) {
-			std::istringstream lineStream(line);
-			lineStream >> characterStats.id >> characterStats.name >> characterStats.health >> characterStats.attack >> characterStats.defence >> characterStats.speed;
-			DebugPrint("ID:%d Name:%s Health:%f Attack:%f Defence:%f Speed:%f", characterStats.id, characterStats.name, characterStats.health, characterStats.attack, characterStats.defence, characterStats.speed);
-		}
-	}*/
-
-	//if (stream.IsGood()) {
-	//	while (std::getline(stream.stream, line)) {
-	//		// Create a stringstream from the line, treating commas as delimiters
-	//		std::istringstream lineStream(line);
-	//		char comma; // To read and discard the commas
-
-	//		lineStream >> characterStats.id >> comma >> characterStats.name >> comma >> characterStats.health >> comma >> characterStats.attack >> comma >> characterStats.defence >> comma >> characterStats.speed;
-
-	//		// Process characterStats or perform other actions as needed
-	//		DebugPrint("ID:%d Name:%s  Health:%f  Attack:%f  Defence:%f  Speed:%f", characterStats.id, characterStats.name.c_str(), characterStats.health, characterStats.attack, characterStats.defence, characterStats.speed);
-	//	}
-	//}
-
-	//while (stream.IsGood()) {
-	//	std::getline(stream.stream,std::stoi(characterStats.id), ',');
-	//}
 	std::getline(stream.stream, header);  // Read the header line and discard it
-		char c;
-		std::string field;
+	char c;
+	std::string field;
 
 	while (!stream.stream.eof()) {
 		field.clear();
@@ -119,12 +86,10 @@ void Serializer::SerializeCSV(const std::string& file) {
 				characterStats.speed = std::stof(field);
 			}
 		}
-		DebugPrint("ID:%d Name:%s  Health:%f  Attack:%f  Defence:%f  Speed:%f", characterStats.id, characterStats.name.c_str(), characterStats.health, characterStats.attack, characterStats.defence, characterStats.speed);
+		//DebugPrint("ID:%d Name:%s  Health:%f  Attack:%f  Defence:%f  Speed:%f", characterStats.id, characterStats.name.c_str(), characterStats.health, characterStats.attack, characterStats.defence, characterStats.speed);
 
-		//// Check for newline to indicate the end of a line
+		// Check for newline to indicate the end of a line
 		if (c == '\n' || stream.stream.eof()) {
-			// You have processed a complete line; do something with characterStats
-			// (e.g., add it to a collection)
 			// Reset characterStats for the next line
 			characterStats = PseudoCharacter();
 		}
