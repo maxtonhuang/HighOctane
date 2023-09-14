4efc83b71033b893e31c3fb300e638ba6d669d4b
wanting-liu <krystaltangtang@gmail.com>
2023-09-14 12:07:59 +0800

    update physics and body

--- ZodiaClash/ZodiaClash/body.cpp
+++ ZodiaClash/ZodiaClash/body.cpp
@@ -1,159 +1,156 @@
-//#include "body.h"
-//#include "physics.h"
-//
-//namespace physics {
-//	Body::Body()
-//	{
-//		Position = Vec2(0, 0);
-//		PrevPosition = Vec2(0, 0);
-//		Velocity = Vec2(0, 0);
-//		Mass = 0.0f;
-//		Acceleration = Vec2(0, 0);
-//		BodyShape = NULL;
-//		Friction = 0.0f;
-//		Restitution = 0.0f;
-//		IsStatic = false;
-//		AccumulatedForce = Vector2(0, 0);
-//	}
-//
-//	Body::~Body()
-//	{
-//		delete BodyShape;
-//		PHYSICS->Bodies.erase(this);
-//	}
-//
-//	void Body::Integrate(float dt)
-//	{
-//		//Do not integrate static bodies
-//		if (IsStatic) return;
-//
-//		//Store prev position
-//		PrevPosition = Position;
-//
-//		//Integrate the position using Euler 
-//		Position = Position + Velocity * dt; //acceleration term is small
-//
-//		//Determine the acceleration
-//		Acceleration = PHYSICS->Gravity;
-//		Vec2 newAcceleration = AccumulatedForce * InvMass + Acceleration;
-//
-//		//Integrate the velocity
-//		Velocity = Velocity + newAcceleration * dt;
-//
-//		//Dampen the velocity for numerical stability and soft drag
-//		Velocity *= std::pow(Damping, dt);
-//
-//		//Clamp to velocity max for numerical stability
-//		if (Dot(Velocity, Velocity) > PHYSICS->MaxVelocitySq)
-//		{
-//			Normalize(Velocity);
-//			Velocity = Velocity * PHYSICS->MaxVelocity;
-//		}
-//
-//		//Clear the force
-//		AccumulatedForce = Vec2(0, 0);
-//	}
-//
-//	void Body::PublishResults()
-//	{
-//		tx->Position = Position;
-//	}
-//
-//	void Body::DebugDraw()
-//	{
-//		if (IsStatic)
-//		{
-//			//White
-//			Drawer::Instance.SetColor(Vec4(1, 1, 1, 1));
-//
-//			//Draw the shape of the object
-//			BodyShape->Draw();
-//		}
-//		else
-//		{
-//			//Red
-//			Drawer::Instance.SetColor(Vec4(1, 0, 0, 1));
-//
-//			//Draw the shape of the object
-//			BodyShape->Draw();
-//
-//			//Draw the velocity of the object
-//			Drawer::Instance.SetColor(Vec4(1, 1, 1, 1));
-//			Drawer::Instance.MoveTo(Position);
-//			Drawer::Instance.LineTo(Position + Velocity * 0.25f);
-//		}
-//
-//	}
-//
-//	void Body::Initialize()
-//	{
-//		//Get the transform to write results to
-//		tx = GetOwner()->has(Transform);
-//
-//		//Get the starting position
-//		Position = tx->Position;
-//
-//		//Add this body to the body list
-//		PHYSICS->Bodies.push_back(this);
-//
-//		//If mass is zero object is interpreted
-//		//to be static
-//		if (Mass > 0.0f)
-//		{
-//			IsStatic = false;
-//			InvMass = 1.0f / Mass;
-//		}
-//		else
-//		{
-//			IsStatic = true;
-//			InvMass = 0.0f;
-//		}
-//
-//		BodyShape->body = this;
-//	}
-//
-//	void Body::Serialize(ISerializer& stream)
-//	{
-//		StreamRead(stream, Mass);
-//		StreamRead(stream, Friction);
-//		StreamRead(stream, Restitution);
-//
-//		std::string shapeName;
-//		StreamRead(stream, shapeName);
-//
-//		if (shapeName == "Circle")
-//		{
-//			ShapeCircle* shape = new ShapeCircle();
-//			StreamRead(stream, shape->Radius);
-//			this->BodyShape = shape;
-//		}
-//
-//		if (shapeName == "Box")
-//		{
-//			ShapeAAB* shape = new ShapeAAB();
-//			StreamRead(stream, shape->Extents);
-//			this->BodyShape = shape;
-//		}
-//
-//	}
-//
-//	void Body::AddForce(Vec2 force)
-//	{
-//		AccumulatedForce += force;
-//	}
-//
-//	void Body::SetPosition(Vec2 p)
-//	{
-//		Position = p;
-//		tx->Position = p;
-//	}
-//
-//	void Body::SetVelocity(Vec2 v)
-//	{
-//		Velocity = v;
-//	}
-//
-//	bool::CheckCollision(const Body& otherBody) {
-//		return CollisionManager::CheckCollision(*this, otherBody);
-//	};
+#include "body.h"
+#include "physics.h"
+
+namespace physics {
+	Body::Body()
+	{
+		position = Vec2(0, 0);
+		prevPosition = Vec2(0, 0);
+		velocity = Vec2(0, 0);
+		acceleration = Vec2(0, 0);
+		accumulatedForce = Vector2(0, 0);
+		mass = 0.0f;
+		restitution = 0.0f;
+		friction = 0.0f;
+		isStatic = false;
+	}
+
+	Body::~Body()
+	{
+		delete bodyShape;
+		PHYSICS->Bodies.erase(this);
+	}
+
+	void Body::Integrate(float deltaTime)
+	{
+		// If the body is static, we don't want to update its position or velocity.
+		if (isStatic) return;
+
+		// Store the current position as the previous position
+		prevPosition = position;
+
+		// Update the position base on deltatime
+		position += velocity * dt;
+
+		// Update the acceleration based on the global gravity and any accumulated forces on the body.
+		acceleration = PHYSICS->gravity;
+		vmath::Vector2 newAcceleration = accumulatedForce + acceleration;
+
+		// Update the velocity using the newly computed acceleration.
+		//velocity is speed with direction
+		velocity += newAcceleration * dt;
+
+		// Ensure the velocity doesn't exceed a maximum value for numerical stability.
+		if (velocity.dot(velocity) > PHYSICS->maxVelocitySq) // Use dot product to get the squared magnitude
+		{
+			velocity.normalize(); // Make the velocity a unit vector
+			velocity *= PHYSICS->maxVelocity; // Scale it to the maximum allowed velocity
+		}
+
+		// Reset the accumulated force to zero for the next frame
+		accumulatedForce = vmath::Vector2(0, 0);
+	}
+
+	void Body::AddForce(vmath::Vector2 force)
+	{
+		accumulatedForce += force;
+	}
+
+	void Body::SetPosition(vmath::Vector2 pos)
+	{
+		position = pos;
+	}
+
+	void Body::SetVelocity(vmath::Vector2 vel)
+	{
+		velocity = vel;
+	}
+
+	bool Body::CheckCollision(const Body& otherBody) 
+	{
+		return CollisionManager::CheckCollision(*this, otherBody);
+	};
+
+
+//void Body::PublishResults()
+//{
+//	tx->Position = Position;
 //}
+
+//void Body::DebugDraw()
+//{
+//	if (IsStatic)
+//	{
+//		//White
+//		Drawer::Instance.SetColor(Vec4(1, 1, 1, 1));
+
+//		//Draw the shape of the object
+//		BodyShape->Draw();
+//	}
+//	else
+//	{
+//		//Red
+//		Drawer::Instance.SetColor(Vec4(1, 0, 0, 1));
+
+//		//Draw the shape of the object
+//		BodyShape->Draw();
+
+//		//Draw the velocity of the object
+//		Drawer::Instance.SetColor(Vec4(1, 1, 1, 1));
+//		Drawer::Instance.MoveTo(Position);
+//		Drawer::Instance.LineTo(Position + Velocity * 0.25f);
+//	}
+
+//}
+
+//void Body::Initialize()
+//{
+//	//Get the transform to write results to
+//	tx = GetOwner()->has(Transform);
+
+//	//Get the starting position
+//	Position = tx->Position;
+
+//	//Add this body to the body list
+//	PHYSICS->Bodies.push_back(this);
+
+//	//If mass is zero object is interpreted
+//	//to be static
+//	if (Mass > 0.0f)
+//	{
+//		IsStatic = false;
+//		InvMass = 1.0f / Mass;
+//	}
+//	else
+//	{
+//		IsStatic = true;
+//		InvMass = 0.0f;
+//	}
+
+//	BodyShape->body = this;
+//}
+
+	void Body::Serialize(ISerializer& stream)
+	{
+		StreamRead(stream, Mass);
+		StreamRead(stream, Friction);
+		StreamRead(stream, Restitution);
+
+		std::string shapeName;
+		StreamRead(stream, shapeName);
+
+		if (shapeName == "Circle")
+		{
+			ShapeCircle* shape = new ShapeCircle();
+			StreamRead(stream, shape->Radius);
+			this->BodyShape = shape;
+		}
+
+		if (shapeName == "Box")
+		{
+			ShapeAAB* shape = new ShapeAAB();
+			StreamRead(stream, shape->Extents);
+			this->BodyShape = shape;
+		}
+
+	}
--- ZodiaClash/ZodiaClash/physics.h
+++ ZodiaClash/ZodiaClash/physics.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //#pragma once
 //#include <vector>
 //#include "VMath.h"
@@ -37,3 +38,50 @@
 //	// global pointer
 //	extern PhysicsManager* PHYSICS;
 //}
+=======
+#pragma once
+#include <vector>
+#include "vmath.h"
+#include "collision.h"
+#include "body.h"
+
+/*
+* TODO:
+* - init
+* - update body position
+* - update forces
+* - consider debug drawing step
+*/
+namespace physics {
+	class PhysicsManager {
+	public:
+		PhysicsManager(); 
+
+	public:
+		void Initialize();
+		void Update(float deltaTime);
+
+	private:
+		void IntegrateBodies(float deltaTime);
+		void DetectCollision(float deltaTime);
+		void Step(float deltaTime);
+		//void DebugDraw(float deltaTime);
+		//void PublishResult();
+		//CollisionManager collision;
+
+	public:
+		std::vector<Body> bodies; // store all the bodies
+		vmath::Vector2 gravity;
+		float maxVelocity;
+		float maxVelocitySq;
+		bool advanceStep;
+		bool stepModeActive;
+		float timeAccumulation;
+		float penetrationEpsilon;
+		//Position correction resolve percentage
+		float penetrationResolvePercentage;
+	};
+	// global pointer
+	extern PhysicsManager* PHYSICS;
+}
+>>>>>>> Stashed changes
