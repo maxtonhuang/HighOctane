95dca76a08bb2f9672deedc347470c1228cf106c
KWenYuan <93873311+KWenYuan@users.noreply.github.com>
2023-09-18 22:12:47 +0800

    Updated function names
    Updated all the functions name, added 2 new files

---
+++ ZodiaClash/ZodiaClash/DebugProfile.h
@@ -0,0 +1,25 @@
+/*
+\copyright
+		All content(C) 2023 DigiPen Institute of Technology Singapore.All rights
+		reserved.Reproduction or disclosure of this file or its contents without the prior
+		written consent of DigiPen Institute of Technology is prohibited.
+*/
+/*!
+@file		DebugProfile.h
+@author		Koh Wen Yuan
+@Email		k.wenyuan@digipen.edu
+@course		CSD 2401
+@section	Section A
+@date		18 September 2023
+@brief		This file contains the functions declaration for debugging
+
+
+TODO :
+PUT THE PERFORMANCE() AT THE END OF THE GAME LOOP
+
+*//*______________________________________________________________________*/
+#pragma once
+
+namespace debugprofile {
+
+}
