01793d159b2be16882fb91b1a356d30792b7b81a
wanting-liu <krystaltangtang@gmail.com>
2023-09-25 00:30:54 +0800

    collision resolution draft

--- ZodiaClash/ZodiaClash/CollisionResolution.h
+++ ZodiaClash/ZodiaClash/CollisionResolution.h
@@ -1,3 +1,4 @@
+#pragma once
 /******************************************************************************
 *
 *	\copyright
@@ -8,7 +9,7 @@
 *
 * *****************************************************************************
 *
-*	@file		Body.h
+*	@file		CollsionResolution.h
 *
 *	@author		Liu Wan Ting
 *
@@ -25,44 +26,16 @@
 *
 *	@brief		Body system of the engine
 *
-*	This file contains functions used in the main body system of the engine
+*	This file contains functions used for collision response
 *
 ******************************************************************************/
 
 #pragma once
-#include "vmath.h"
-#include "Collision.h"
-#include "Physics.h"
-
-using namespace vmath;
-
-//class Shape; //forward declaration
+#include "body.h"
+#include "collision.h"
 
 namespace physics {
-
-	struct Body {
-		Body* next{};
-		Body* prev{};
-		Vector2 position{};
-		Vector2 prevPosition{};
-		Vector2 velocity{};
-		Vector2 acceleration{};
-		Vector2 accumulatedForce{};
-		float mass{};
-		float restitution{};
-		float friction{ 0.2f };
-		bool isStatic{};
-		Shape* bodyShape;
-
-		// Constructors, if needed, can remain
-		Body();
-
-		// These setters might stay if you find them useful, but in pure ECS they might not be here.
-		void SetPosition(Vector2 pos);
-		void SetVelocity(Vector2 vel);
-
-		// Getters can also be useful
-		Vector2 GetPosition() const { return position; }
-		Vector2 GetVelocity() const { return velocity; }
-	};
+    void DynamicStaticResponse(Body& dynamicBody, const Body& staticBody);
+    void DynamicDynamicResponse(Body& bodyA, Body& bodyB);
+    void HandleCollisionResponse(Body& alpha, Body& beta);
 }
--- ZodiaClash/ZodiaClash/physics.cpp
+++ ZodiaClash/ZodiaClash/physics.cpp
@@ -1,3 +1,34 @@
+/******************************************************************************
+*
+*	\copyright
+*		All content(C) 2023/2024 DigiPen Institute of Technology Singapore.
+*		All rights reserved. Reproduction or disclosure of this file or its
+*		contents without the prior written consent of DigiPen Institute of
+*		Technology is prohibited.
+*
+* *****************************************************************************
+*
+*	@file		Physics.cpp
+*
+*	@author		Liu Wan Ting
+*
+*	@email		wanting.liu@digipen.edu
+*
+*	@course		CSD 2401 - Software Engineering Project 3
+*				CSD 2451 - Software Engineering Project 4
+*
+*	@section	Section A
+*
+*	@date		24 September 2023
+*
+* *****************************************************************************
+*
+*	@brief		Physics system of the engine
+*
+*	This file contains functions used in the main physics system of the engine
+*
+******************************************************************************/
+
 #include "Physics.h"
 #include <math.h>
 
@@ -133,59 +164,4 @@ namespace physics {
             graphics.DrawLine(body.position.x, body.position.y, endPosition.x, endPosition.y);
         }
     }
-
-
-    //void CollisionManager::CollisionDetectionAndResponse() {
-    //    //for each body, check for collision with the all other bodies in the container
-    //    for (size_t i = 0; i < bodies.size(); ++i) {
-    //        for (size_t j = i + 1; j < bodies.size(); ++j) {
-    //            //if collied, execute collsion response
-    //            if (CheckCollision(bodies[i], bodies[j])) {
-    //                CollisionResponse(bodies[i], bodies[j]);
-    //            }
-    //        }
-    //    }
-    //}
-
-    //bool CollisionManager::CheckCollision(const Body& bodyA, const Body& bodyB) {
-    //    // Compare AABBs (Axis-Aligned Bounding Boxes) of the bodies
-    //    float half_sumWidth = (bodyA.size.x + bodyB.size.x) * 0.5f, half_sumHeight = (bodyA.size.y + bodyB.size.y) * 0.5f;
-
-    //    // Horizontal: CHeck the difference between the x coordinate of the body's center positions and bodies' width
-    //    bool xOverlap = abs(bodyA.position.x - bodyB.position.x) <= half_sumWidth; //overlap if less than half width
-
-    //    // Vertical: CHeck the difference between the y coordinate of the body's center positions and bodies' heigh
-    //    bool yOverlap = abs(bodyA.position.y - bodyB.position.y) <= half_sumHeight; //overlap if less than half height
-
-    //    // if both x and y axes overlap, a collision is detected, return true
-    //    return xOverlap && yOverlap;
-    //}
-
-
-    //void CollisionManager::CollisionResponse(Body& bodyA, Body& bodyB) {
-    //    // Calculate relative velocity , how the bodies are moving relative to each other after the collision
-    //    vmath::Vector2 relativeVelocity = bodyB.velocity - bodyA.velocity;
-
-    //    // Calculate the collision direction vector (direction from A to B), normalized so length = 1
-    //    vmath::Vector2 collisionDirection = (bodyB.position - bodyA.position).normalize();
-
-    //    //measure how fast the bodies are moving towards each other along the direction of collision
-    //    float velocityAlongDirection = relativeVelocity ^ (collisionDirection);
-
-    //    // checking if the relative motion is away from the collision point (bodies moving away from each other)
-    //    if (velocityAlongDirection > 0) {
-    //        return; // bodies are already separating, no need to resolve (not gonna colide)
-    //    }
-
-    //    // use the bounciness of both bodies to calculate the scalar value to be applied 
-    //    // for collision response IN OPPOSITE DIRECTION hence -ve (0 = no bounce, 1 = full bounce)
-    //    float bounceScalar = -(std::min(bodyA.bounciness, bodyB.bounciness)) * velocityAlongDirection;
-    //    bounceScalar /= (1 / bodyA.mass + 1 / bodyB.mass); //adjusts the impulse scalar based on the masses of the bodies
-
-    //    // Apply rebounce to each body based on mass and collision direction
-    //    vmath::Vector2 bounce = collisionDirection * bounceScalar;
-    //    bodyA.velocity -= bounce / bodyA.mass;
-    //    bodyB.velocity += bounce / bodyB.mass;
-
-    //}
 }
