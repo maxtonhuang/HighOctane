add0328a4584390c185d99c75faae0cb40558673
Antisheeps <112918823+Antisheeps@users.noreply.github.com>
2023-10-18 21:04:11 +0800

    framebuffer comments

--- ZodiaClash/HighOctane_CustomEngine/FrameBuffer.cpp
+++ ZodiaClash/HighOctane_CustomEngine/FrameBuffer.cpp
@@ -1,3 +1,36 @@
+/******************************************************************************
+*
+*	\copyright
+*		All content(C) 2023/2024 DigiPen Institute of Technology Singapore.
+*		All rights reserved. Reproduction or disclosure of this file or its
+*		contents without the prior written consent of DigiPen Institute of
+*		Technology is prohibited.
+*
+* *****************************************************************************
+*
+*	@file		FrameBuffer.cpp
+*
+*	@author		Foong Pun Yuen Nigel
+*
+*	@email		p.foong\@digipen.edu
+*
+*	@course		CSD 2401 - Software Engineering Project 3
+*				CSD 2451 - Software Engineering Project 4
+*
+*	@section	Section A
+*
+*	@date		18 October 2023
+*
+* *****************************************************************************
+*
+*	@brief
+*
+*	This file contains functions for game frame buffer
+*	Entire game is rendered as a frame buffer texture and then
+*	drawn to the screen
+*
+******************************************************************************/
+
 #include "FrameBuffer.h"
 #include "debugdiagnostic.h"
 
@@ -14,10 +47,10 @@ void FrameBuffer::Initialize() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureid, 0);
 
+	//create depth and stencil buffer
 	GLuint rbo;
 	glCreateTextures(GL_TEXTURE_2D, 1, &rbo);
 	glBindTexture(GL_TEXTURE_2D, rbo);
-	//glTexStorage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, GRAPHICS::defaultWidth, GRAPHICS::defaultHeight);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, GRAPHICS::defaultWidth, GRAPHICS::defaultHeight, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, rbo, 0);
 
