185dbe97cd80448ab32649cdb511fae3d6b634f0
Antisheeps <112918823+Antisheeps@users.noreply.github.com>
2023-10-30 23:30:54 +0800

    audio events

--- ZodiaClash/HighOctane_CustomEngine/Events.cpp
+++ ZodiaClash/HighOctane_CustomEngine/Events.cpp
@@ -20,7 +20,31 @@ void PlayAudio(std::string input) {
 	//Find the entity from map using input string
 	//Call the sound component and play it
 	DEBUG_PRINT("playing: %s", input.c_str());
-	assetmanager.audio.PlaySounds(input.c_str(), "SFX");
+	assetmanager.audio.PlaySounds(input.c_str(),"SFX");
+}
+void PlayMusic(std::string input) {
+	//Find the entity from map using input string
+	//Call the sound component and play it
+	DEBUG_PRINT("playing: %s", input.c_str());
+	assetmanager.audio.PlaySounds(input.c_str(), "BGM");
+}
+void RestartMusic(std::string input) {
+	//Find the entity from map using input string
+	//Call the sound component and play it
+	assetmanager.audio.StopGroup("BGM");
+	DEBUG_PRINT("playing: %s", input.c_str());
+	assetmanager.audio.PlaySounds(input.c_str(), "BGM");
+}
+void PauseResumeGroup(std::string input) {
+	if (assetmanager.audio.IsGroupPaused(input.c_str())) {
+		assetmanager.audio.ResumeGroup(input.c_str());
+	}
+	else {
+		assetmanager.audio.PauseGroup(input.c_str());
+	}
+}
+void StopGroup(std::string input) {
+	assetmanager.audio.StopGroup(input.c_str());
 }
 void TestFunction(std::string input) {
 	std::cout << input << "\n";
@@ -28,7 +52,11 @@ void TestFunction(std::string input) {
 
 void EventManager::InitialiseFunctions() {
 	//functions["ChangeLevel"] = ChangeLevel;
-	functions["Audio"] = PlayAudio;
+	functions["Play Sound"] = PlayAudio;
+	functions["Play Music"] = PlayMusic;
+	functions["Restart Music"] = RestartMusic;
+	functions["Pause/Resume Group"] = PauseResumeGroup;
+	functions["Stop Group"] = StopGroup;
 	functions["Exit Game"] = ExitGame;
 	functions["Change Scene"] = ChangeScene;
 	functions["Test"] = TestFunction;
