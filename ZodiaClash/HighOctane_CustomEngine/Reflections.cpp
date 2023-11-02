49bac55185579dd3b53e091badce01507eea8395
KWenYuan <93873311+KWenYuan@users.noreply.github.com>
2023-11-02 22:27:25 +0800

    Own RTTR Library v0.6 (WIP), Updated project guide
    Thinking of how to implement the RTTR library to fit the rubrics

--- ZodiaClash/HighOctane_CustomEngine/Reflections.cpp
+++ ZodiaClash/HighOctane_CustomEngine/Reflections.cpp
@@ -1,3 +1,32 @@
+/******************************************************************************
+*
+*	\copyright
+*		All content(C) 2023/2024 DigiPen Institute of Technology Singapore.
+*		All rights reserved. Reproduction or disclosure of this file or its
+*		contents without the prior written consent of DigiPen Institute of
+*		Technology is prohibited.
+*
+* *****************************************************************************
+*
+*	@file		Reflections.cpp
+*
+*	@author		Koh Wen Yuan
+*
+*	@email		k.wenyuan\@digipen.edu
+*
+*	@course		CSD 2401 - Software Engineering Project 3
+*				CSD 2451 - Software Engineering Project 4
+*
+*	@section	Section A
+*
+*	@date		20 October 2023
+*
+* *****************************************************************************
+*
+*	@brief		cpp files for the reflection library
+*
+*	This file contains the definition of the functions for the reflection library
+******************************************************************************/
 #include "Reflections.h"
 
 std::vector<VariableInfo> variablesTEST{};
--- ZodiaClash/HighOctane_CustomEngine/Reflections.h
+++ ZodiaClash/HighOctane_CustomEngine/Reflections.h
@@ -1,3 +1,32 @@
+/******************************************************************************
+*
+*	\copyright
+*		All content(C) 2023/2024 DigiPen Institute of Technology Singapore.
+*		All rights reserved. Reproduction or disclosure of this file or its
+*		contents without the prior written consent of DigiPen Institute of
+*		Technology is prohibited.
+*
+* *****************************************************************************
+*
+*	@file		Reflections.h
+*
+*	@author		Koh Wen Yuan
+*
+*	@email		k.wenyuan\@digipen.edu
+*
+*	@course		CSD 2401 - Software Engineering Project 3
+*				CSD 2451 - Software Engineering Project 4
+*
+*	@section	Section A
+*
+*	@date		20 October 2023
+*
+* *****************************************************************************
+*
+*	@brief		header files for the reflection library
+*
+*	This file contains the declaration of the functions for the reflection library
+******************************************************************************/
 #pragma once
 #include <iostream>
 #include <unordered_map>
