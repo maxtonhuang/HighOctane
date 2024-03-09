99357682566707c6b4c133815186febe49cab2f1
Antisheeps <112918823+Antisheeps@users.noreply.github.com>
2024-03-10 03:18:23 +0800

    change goat to 0.5 on death

--- ZodiaClash/HighOctane_CustomEngine/Battle.cpp
+++ ZodiaClash/HighOctane_CustomEngine/Battle.cpp
@@ -355,7 +355,7 @@ void BattleSystem::Update()
                 if (ECS::ecs().GetComponent<Name>(c->entity).name == "Goat_Enemy") {
                     for (auto& character : turnManage.characterList) {
                         if (character.tag == CharacterType::PLAYER) {
-                            character.stats.health += 0.4f * c->stats.maxHealth;
+                            character.stats.health += 0.5f * c->stats.maxHealth;
                             if (character.stats.health > character.stats.maxHealth) {
                                 character.stats.health = character.stats.maxHealth;
                             }
