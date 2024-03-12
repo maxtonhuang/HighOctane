c1309b935cbeedf1208fbda297844b8c14d1aeef
Antisheeps <112918823+Antisheeps@users.noreply.github.com>
2024-03-12 10:35:04 +0800

    fixed voiceover bug

--- ZodiaClash/HighOctane_CustomEngine/input.cpp
+++ ZodiaClash/HighOctane_CustomEngine/input.cpp
@@ -91,7 +91,7 @@ void InputManager::KeyCallback(GLFWwindow* pwin, int key, int scancode, int acti
         if (GLFW_KEY_F8 == key) {
             events.Call("Change Scene", "battle4.scn");
         }
-        if (GLFW_KEY_ENTER) {
+        if (GLFW_KEY_ENTER == key) {
             events.Call("Pause/Resume Group", "VOC");
         }
         if (GLFW_KEY_F11 == key) {
